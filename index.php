dd145e7598eb9de3d67d13d843db591fc32328ca
terromok <terromok@yandex.ru>
2019-12-05 16:26:06 +0300

    Логику изменил. Работает. Дальше буду логированием заниматься

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$n = 7;
+$n = 17;
 $m = 5;
 
 $digits = function(int $length)
@@ -10,7 +10,6 @@ $digits = function(int $length)
         --$length;
     };
 };
-
 
 
 class Sequence
@@ -25,42 +24,56 @@ class Sequence
     }
 
     public function add($element) {
-        array_push($this->seq, "$el");
+        if (count($this->result) < ($this->m - 1) ) {        //если массив для результата еще не полон
+            $this->addInResult($element);                    //то просто добавляем элемент в массив
+        } else if (count($this->result) == ($this->m - 1)) { //если длина массива равна m
+            $this->addInResult($element);                    //то добавляем элемент
+            rsort($this->result);                            //и сортируем по убыванию
+            print_r($this->result);                          //выводит первые данные для проверки
+            echo "<br>";
+        } else {                                                //если массив полон
+            if ($element >= $this->result[$this->m-1]) {        //то делаем проверку. если число больше последнего  элемента
+                for ($i = 0; $i < ($this->m - 1) ; $i++) {      //сверяем с каждым элементом в массиве
+                    if ($element > $this->result[$i]) {         //если добавляемый элемент больше, чем i-тый элемент массива
+                        for ($j = ($this->m - 1); $j > $i ; $j--) {
+                            $this->result[$j] = $this->result[$j -1]; //то все элементы с конца перемещаем на одну позицию вправо
+                            //echo "hhhhhhhh<br>";
+                        }
+                        $this->result[$i] = $element; //добавляемый элемент встает на место i-того элемента
+                        return;
+                    }
+                }
+            }
+        }
+
+    }
+
+    public function addInResult($element) {         //промежуточная функция добавления элемента, пока массив не полон
+        array_push($this->result, "$element");      //чтобы не было дублирования кода
     }
 
     public function getMaxNumbers() {
-
-        rsort($this->seq);
-        $this->log("Последовательность отсортирована по убыванию");
         for ($i=0; $i < $this->m; $i++) {
-            if ($this->seq[$i]) {
-                echo $this->seq[$i];
+            if ($this->result[$i]) {
+                echo $this->result[$i];
                 echo "<br>";
             }
         }
-        $this->log("Вывели максимальные значения последовательности");
-
-    }
-
-    public function log($text) {
-        $fd = fopen("log.txt", 'a+');
-        fwrite($fd, date("d.m.Y h:i:s A : "));
-        fwrite($fd, $text);
-        fwrite ($fd, "\n");
-        fclose($fd);
     }
 
 }
 
 
 $sequence = new Sequence($m);
-$sequence->log("Начало работы приложения");
+//$sequence->log("Начало работы приложения");
 foreach ($digits($n) as $number) {
+                    echo $number;                   //для проверки. можно (даже нужно) закомментировать
+                echo "<br>";
    $sequence->add($number);
 }
-$sequence->log("Создана последовательность из произвольных чисел");
-
+//$sequence->log("Создана последовательность из произвольных чисел");
+echo "------------------------------------<br>";
 print_r($sequence->getMaxNumbers());
-$sequence->log("Конец работы приложения");
-$sequence->log("------------------------------------------------");
+//$sequence->log("Конец работы приложения");
+//$sequence->log("------------------------------------------------");
 
